0a3181de968ea8c1ee81cfbea981be24184f2eac
Artix <artix.2a@gmail.com>
2018-01-20 03:16:55 +0300

    Added favicon to make it interesting

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const compress = require('koa-compress')
+const favicon = require('koa-favicon')
+const path = require('path')
 
 const { errorHandler, auth, getThread, getThreads, getThreadsLight } = require('./modules/middlewares')
 const log = require('./modules/logger')
@@ -16,6 +18,7 @@ router
 
 app
     .use(errorHandler)
+    .use(favicon(path.join(__dirname, 'favicon.ico')))
     .use(auth)
     .use(router.routes())
     .use(compress())
